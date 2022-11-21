d31be4d8b85bb1c0d4f86cbbd1cfc55b84d67e70
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2022-11-21 23:12:39 +0900

    스타일 수정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,56 @@
 import "./App.css";
 import { useEffect, useState } from "react";
-import { Reset } from "styled-reset";
+import styled from "styled-components";
+
+const MenuBox = styled.div`
+  width: 550px;
+  height: 500px;
+  max-width: 550px;
+  background-color: antiquewhite;
+  text-align: center;
+  border-radius: 5px;
+`;
+
+const MenuTitle = styled.div`
+  margin: 20px 0 10px 0;
+  color: black;
+  font-size: 25px;
+  font-weigth: bold;
+`;
+
+const SubTitle = styled.div`
+  color: black;
+  font-size: 17px;
+`;
+
+const CompanyList = styled.select`
+  width: 400px;
+  height: 45px;
+  margin-top: 30px;
+  border: none;
+  border-radius: 5px;
+  text-align: center;
+`;
+
+const BillingInput = styled.input`
+  width: 400px;
+  width: 400px;
+  height: 45px;
+  margin-top: 30px;
+  border: none;
+  border-radius: 5px;
+  text-align: center;
+`;
+
+const SubmitBtn = styled.button`
+  width: 100px;
+  height: 35px;
+  margin: 30px auto;
+  border: none;
+  border-radius: 5px;
+  text-align: center;
+  display: block;
+`;
 
 function App() {
   const API_KEY = process.env.REACT_APP_API_KEY;
@@ -21,43 +71,36 @@ function App() {
   return (
     <div className="App">
       {modal === true ? <Modal modal={modal} /> : null}
-      <div className="menu-box">
-        <div className="menu-title">메인메뉴</div>
-        <p className="menu-sub">조회할 택배사를 고른 후 운송장 번호를 기입하시오</p>
-      </div>
-      <select className="companylist" onChange={(e) => setChoice(e.target.value)} value={choice}>
-        {company.map((name, i) => {
-          return (
-            <option key={i} value={name.Code}>
-              {name.Name}
-            </option>
-          );
-        })}
-      </select>
-      <form
-        className="waybill"
-        action={`https://info.sweettracker.co.kr/api/v1/trackingInfo?t_code=${choice}&t_invoice=${billing}&t_key=${API_KEY}`}
-        method="post"
-      >
-        <input
+      <MenuBox>
+        <MenuTitle>메인메뉴</MenuTitle>
+        <SubTitle>조회할 택배사를 고른 후 운송장 번호를 기입하시오</SubTitle>
+        <CompanyList className="companylist" onChange={(e) => setChoice(e.target.value)} value={choice}>
+          {company.map((name, i) => {
+            return (
+              <option key={i} value={name.Code}>
+                {name.Name}
+              </option>
+            );
+          })}
+        </CompanyList>
+        <BillingInput
           type="text"
           placeholder="운송장 번호를 입력하시오"
           onInput={(e) => {
             setBilling(e.target.value);
           }}
-        ></input>
-        <div>
-          <button
-            className="btn"
-            onClick={(e) => {
-              e.preventDefault();
-              return billing === undefined ? setModal(true) : setModal(false);
-            }}
-          >
-            운송장 조회
-          </button>
-        </div>
-      </form>
+        ></BillingInput>
+        <SubmitBtn
+          className="btn"
+          onClick={(e) => {
+            e.preventDefault();
+            return billing === undefined ? setModal(true) : setModal(false);
+          }}
+        >
+          운송장 조회
+        </SubmitBtn>
+      </MenuBox>
+      <div></div>
     </div>
   );
 }
